1d545a6120406d57f465dc2ac1a89f2ab777d589
Josh Crawford <josh@crawford.cc>
2022-03-03 15:39:12 -0800

    task: no need for CalHash; use factory:query task

--- deploy/MasterChef.js
+++ deploy/MasterChef.js
@@ -24,15 +24,15 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   }
 
   const chainId = await getChainId();
-  if(chainId === "31337" || chainId === "10001") {
+  if((chainId === "31337" || chainId === "10001") && await sushi.owner() == deployer.address) {
     await (await sushi.mint(dev, 10000000000, txOptions)).wait()
   }
 
-  if (await sushi.owner() !== address) {
-    // Transfer Sushi Ownership to Chef
-    console.log("Transfer Sushi Ownership to Chef")
-    await (await sushi.transferOwnership(address, txOptions)).wait()
-  }
+  // if (await sushi.owner() !== address) {
+  //   // Transfer Sushi Ownership to Chef
+  //   console.log("Transfer Sushi Ownership to Chef")
+  //   await (await sushi.transferOwnership(address, txOptions)).wait()
+  // }
 
   const masterChef = await ethers.getContract("MasterChef")
   if (await masterChef.owner() !== dev) {
