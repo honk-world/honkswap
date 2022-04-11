14c43e8cb9d8732db964f4fe5860a82e8b36061f
Josh Crawford <josh@crawford.cc>
2022-04-11 14:53:39 -0700

    chore: fix rewards

--- deploy/MasterChef.js
+++ deploy/MasterChef.js
@@ -14,10 +14,10 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
     rewardToken.address = HONK_ADDRESS[chainId]
   }
   
-  const startBlock = 3926552
+  const startBlock = 4000000
   const endBlock = startBlock + 1 // effectively eliminate this
   const rewardBlocks = [2827620+startBlock, 5655240+startBlock, 8482860+startBlock, 11310480+startBlock, 14138100+startBlock, 16965720+startBlock]
-  const rewards = [9549, 4244, 1273, 354, 124, 88]
+  const rewards = [954967, 424485, 127316, 35465, 12478, 8841]
   const { address } = await deploy("MasterChef", {
     from: deployer,
     args: [rewardToken.address, dev, startBlock, endBlock, 
